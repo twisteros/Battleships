3743bdbe7418e855f976cd396a9b11feea3ad5c0
twisteros <twisteros@users.noreply.github.com>
2023-07-02 22:38:25 +0200

    Update BattlefieldCell.cs

--- BattlefieldCell.cs
+++ BattlefieldCell.cs
@@ -1,10 +1,11 @@
-﻿namespace Battleships
+namespace Battleships
 {
     internal class BattlefieldCell
     {
         public string VCoordinate { get; set; }
         public string HCoordinate { get; set; }
         public ShipSquare ShipSquare { get; set; }
+        public ShipBase Ship {get; set;}
         public bool Revealed { get; set; }
     }
 }
