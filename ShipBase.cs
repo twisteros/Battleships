6aae6295e06ac0225ecdf3060915b19fddfb18d7
twisteros <twisteros@users.noreply.github.com>
2023-07-02 22:38:03 +0200

    Update ShipBase.cs

--- ShipBase.cs
+++ ShipBase.cs
@@ -1,16 +1,11 @@
-﻿using System.Drawing;
-
 namespace Battleships
 {
     public abstract class ShipBase
     {
         abstract public string Name { get; }
         abstract public int Length { get; }
-        //private Point[] _shape;
-        //public Point[] Shape => _shape ?? (_shape = new Point[Length]);
-        //private Point[] _coordinates;
-        //public Point[] Coordinates => _coordinates ?? (_coordinates = new Point[Length]);
         private ShipSquare[] _squares;
         public ShipSquare[] Squares => _squares ?? (_squares = new ShipSquare[Length]);
+        public bool Sunk { get; set; }
     }
 }
